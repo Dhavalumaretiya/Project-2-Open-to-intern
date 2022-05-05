129e95bb8068dbd3a81c69d04f147d26e20d197a
unknown <123@123-PC>
2022-05-05 15:37:24 +0530

    json

--- src/Controller/allcontroller.js
+++ src/Controller/allcontroller.js
@@ -4,8 +4,8 @@ const collageModel = require("../models/collegeModel")
 
 let postcreate = async function(req,res){
     let requestBody = req.body;
-        let collegeID = await collageModel.findOne({ name: requestBody.collegeName }).select({name:1 , _id:1 , fullname:0, logolink:0});
-        requestBody['collegeId'] = collegeID;
+        let collegeID = await collageModel.find({ name: requestBody.collegeName })
+        requestBody['collageId'] = collegeID;
         let Data = await internModel.create(requestBody);
         res.status(201).send({ status: true, msg: "Created", Data: Data });
 }
